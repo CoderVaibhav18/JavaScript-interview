3bbb5be2ff4f3f975d611836913ba1733caea99d
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-02-08 23:56:09 +0530

    firats commiot]

---
+++ Interval.js
@@ -0,0 +1,19 @@
+// print all array element in reverse order and each element after dekay 1s
+// SOUL AI
+
+let data = [1,2,3,4,5];
+
+// let interval = setInterval(() => {
+//   console.log(data.pop());
+//   if(interval && data.length === 0){
+//     clearInterval(interval);
+//   }
+// }, 1000)
+let p = 1;
+
+for(let i = data.length - 1; i >= 0; i--){
+  setTimeout(() => {
+    console.log(data[i]);
+  }, p * 1000)
+  p++
+}
